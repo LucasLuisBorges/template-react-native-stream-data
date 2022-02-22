753eddf2f427db53484b9c4ee04a51f9141663b0
Lucas Luis Borges <lucasluisborges@outlook.com>
2022-02-22 12:04:27 -0300

    Parte 1

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -37,15 +37,36 @@ function AuthProvider({ children }: AuthProviderData) {
   const [userToken, setUserToken] = useState('');
 
   // get CLIENT_ID from environment variables
+  const { CLIENT_ID } = process.env
+
+  useEffect(() => {
+    api.defaults.headers['Client-Id'] = CLIENT_ID;
+  }, [])
 
   async function signIn() {
     try {
-      // set isLoggingIn to true
+      setIsLoggingIn(true);
 
-      // REDIRECT_URI - create OAuth redirect URI using makeRedirectUri() with "useProxy" option set to true
+      const REDIRECT_URI = makeRedirectUri({ useProxy: true });
+      const RESPONSE_TYPE = 'token';
+      const SCOPE = encodeURI('openid user:read:email user:read:follows');
+      const FORCE_VERIFY = true;
+      const STATE = generateRandom(30);
+
+      const authUrl = twitchEndpoints.authorization + 
+      `?client_id=${CLIENT_ID}` + 
+      `&redirect_uri=${REDIRECT_URI}` + 
+      `&response_type=${RESPONSE_TYPE}` + 
+      `&scope=${SCOPE}` + 
+      `&force_verify=${FORCE_VERIFY}` +
+      `&state=${STATE}`;
+
       // RESPONSE_TYPE - set to "token"
+
       // SCOPE - create a space-separated list of the following scopes: "openid", "user:read:email" and "user:read:follows"
+
       // FORCE_VERIFY - set to true
+      
       // STATE - generate random 30-length string using generateRandom() with "size" set to 30
 
       // assemble authUrl with twitchEndpoint authorization, client_id, 
